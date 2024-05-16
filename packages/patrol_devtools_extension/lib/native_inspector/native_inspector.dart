c06e7a11c37219d2d21bd04acdfdd8f542932483
nilsreichardt <me@nils.re>
2024-05-16 12:38:25 +0200

    Hide Split from `material.dart`

--- packages/patrol_devtools_extension/lib/native_inspector/native_inspector.dart
+++ packages/patrol_devtools_extension/lib/native_inspector/native_inspector.dart
@@ -1,5 +1,5 @@
 import 'package:devtools_app_shared/ui.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Split;
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:patrol_devtools_extension/native_inspector/native_view_details.dart';
 import 'package:patrol_devtools_extension/native_inspector/native_view_hierarchy.dart';
